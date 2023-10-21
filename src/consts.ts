10f83f9842e518a466e8341e8f47175d0ecb73e1
Pinapelz <yukais@pinapelz.com>
2023-10-21 00:12:54 -0700

    update site name - from site -> blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "pinapelz's site";
-export const SITE_DESCRIPTION = 'Welcome to Pinapelz\'s Site!';
+export const SITE_TITLE = "pinapelz's blog";
+export const SITE_DESCRIPTION = 'Welcome to Pinapelz\'s Blog!';

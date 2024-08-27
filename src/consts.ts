e3f0281c25d6ead23264a94b0a7b18f10019c185
Pinapelz <yukais@pinapelz.com>
2024-08-26 20:23:17 -0700

    fix: rss.xml endpoint API route handler

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "pinapelz's blog";
-export const SITE_DESCRIPTION = 'Welcome to Pinapelz\'s Blog!';
+export const SITE_DESCRIPTION = 'A lot of rambling about a bunch of random things, both technical and non-technical.';
